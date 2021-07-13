00849b58e864ca4fb54e24d373a08639b35273c6
Partha Sen <partha1189@gmail.com>
2021-07-13 22:53:00 +0530

    Timeline getPhotos with custom hook --Insta

--- src/components/timeline.js
+++ src/components/timeline.js
@@ -1,7 +1,25 @@
+/* eslint-disable no-nested-ternary */
+import Skeleton from 'react-loading-skeleton';
+import usePhotos from '../hooks/use-photos';
+
 export default function Timeline() {
+  // get logged in users photos (hook)
+  const { photos } = usePhotos();
+  // react skeleton for loadin photos
+  // if we have a photo, render them (create post component)
+  // if user doest have photos ask them to create some photos
+
   return (
     <div className="container col-span-2">
-      <p>I am timeline</p>
+      {!photos ? (
+        <>
+          <Skeleton count={4} width={640} height={500} className="mb-5" />
+        </>
+      ) : photos?.length > 0 ? (
+        photos.map((content) => <p key={content.docId}>{content.imageSrc}</p>)
+      ) : (
+        <p className="text-center text-2xl">Follow people to see photos!</p>
+      )}
     </div>
   );
 }
